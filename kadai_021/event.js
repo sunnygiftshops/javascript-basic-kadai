de100b76cea86a90bf535f26cb95041bbd554823
sunnygiftshops <sunnygiftshops@gmail.com>
2024-08-13 10:15:39 +0900

    課題　非同期処理を使用して、2秒後に文章を書き換えよう

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn =document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click',() => {
+    console.log('ボタンをクリックしました');
+
+    // 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+    console.log('非同期処理');
+  }, 2000);
+});
